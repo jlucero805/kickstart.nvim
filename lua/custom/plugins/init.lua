32c12c2f0434a6a05d4afa10d4f4f2a7f8e272f5
jlucero805 <jlucero805@gmail.com>
2024-08-04 10:28:43 -0400

    Add custom key maps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+vim.keymap.set('n', '-', function()
+  require('oil').open_float(vim.fn.getcwd())
+end)
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
+  {
+    'tpope/vim-fugitive',
+  },
+}
